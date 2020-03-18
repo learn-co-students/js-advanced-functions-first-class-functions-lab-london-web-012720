2106f41f877712b107cb33545b7ff4510c4ecc6f
alexandre jamarco <alexandre.j@gmail.com>
2020-03-18 11:58:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(arr){
+    return arr.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(arr){
+    return arr.slice(arr.length - 2, arr.length);
+}
+
+// const days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+// console.log(returnLastTwoDrivers(days));
+
+const selectingDrivers = [ returnFirstTwoDrivers, returnLastTwoDrivers ];
+
+function createFareMultiplier(multiply_value){
+    return function(value){
+        return value * multiply_value;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(arrayOfDrivers, func){
+    if (func === returnFirstTwoDrivers){
+        return returnFirstTwoDrivers(arrayOfDrivers);
+    }else if (func === returnLastTwoDrivers){
+        return returnLastTwoDrivers(arrayOfDrivers);
+    }
+}
